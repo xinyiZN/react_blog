1af0fe56192b11ed81f1f1f701c39fee9f672bd2
xinyizhou <1838435256@qq.com>
2025-02-12 23:35:24 +0800

    修改配置

--- vite-blog/vite.config.ts
+++ vite-blog/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path' //导入Path模块
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 路径别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // 配置 @ 别名指向 src 文件夹
+    }
+  }
 })
